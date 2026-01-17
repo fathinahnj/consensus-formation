e4bd7f3d54b58487dc7860826de6158369906463
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 16:02:48 +0700

    add agent interaction ground rule (if agents can interact in the first place) using confidence threshold

--- src/agent.py
+++ src/agent.py
@@ -2,7 +2,19 @@ class Agent:
   def __init__(self, agent_id, opinion, confidence, open_mindedness, stubbornness):
     self.id = agent_id,
     self.opinion = opinion,
-    self.confidence = confidence,
+    self.confidence = confidence, # for dynamic threshold
     self.open_mindedness = open_mindedness,
     self.stubbornness = stubbornness
+    
+    
+  def can_interact(self, other, confidence_threshold):
+    '''
+    Docstring for can_interact
+    
+    :param self: agent
+    :param other: other agent
+    :param confidence_threshold: implementation of bounded confidence rule (Deffuant / Hegselmann–Krause)
+    :return: boolean indicating if interaction is possible (if opinion is not too different)       
+    '''
+    return abs(self.opinion - other.opinion) <= confidence_threshold
     
