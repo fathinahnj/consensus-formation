096a528cebf3c1c16cc1f75b1bc054ce2bd2b1a0
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 16:26:34 +0700

    retrieve random_seed from config for seed handling

--- src/simulation.py
+++ src/simulation.py
@@ -1,3 +1,6 @@
+import random
+import numpy as np
+
 class Simulation:
   def __init__(self, config):
     self.config = config,
@@ -5,4 +8,5 @@ class Simulation:
     self.network = None,
     self.history = []
     
-    
+    random.seed(config["simulation"]["random_seed"])
+    np.random.seed(config["simulation"]["random_seed"])
